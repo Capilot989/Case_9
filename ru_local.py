3f6046b24f2e3d7227f6b0882d5cb0af168537e3
Capilot989 <capitan22890@gmail.com>
2025-10-21 15:45:10 +0700

    Create ru_local.py

---
+++ ru_local.py
@@ -0,0 +1,17 @@
+ENTER_NUMBER = 'Введите количество шестиугольников, от 4 до 20: '
+RETRY_ENTER_NUMBER = 'Пожалуйста введите число в диапазоне от 4 до 20: '
+
+AVAILABLE_COLORS = 'Доступные цвета:'
+AQUAMARINE = 'Аквамариновый' 
+SKY_BLUE = 'Небесно синий'
+CADET_BLUE = 'Кадетский синий'
+CHARTREUSE = 'Шартрез (желто-зеленый)'
+CORNFLOWERBLUE = 'Васильковый'
+DARKORCHID = 'Темная орхидея'
+CYAN = 'Циан'
+
+ENTER_COLOR = 'Введите цифру выбранного цвета: '
+RETRY_ENTER_COLOR = 'Введите корректное число из списка'
+
+ENTER_FIRST_COLOR = 'Выберите первый цвет'
+ENTER_SECOND_COLOR = 'Выберите второй цвет'
